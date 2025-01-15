3d408b2982840eef83679a4105f770bb9a26c8e2
Iñaki Arango <arangoinaki@gmail.com>
2025-01-15 13:42:16 -0800

    Biocypher initial code

--- src/optimuskg/pipelines/gold/pipeline.py
+++ src/optimuskg/pipelines/gold/pipeline.py
@@ -13,4 +13,13 @@ def create_pipeline(**kwargs):
     return pipeline(
         [getattr(nodes, name) for name in nodes.__all__],
         namespace="gold",
+        inputs=[
+            "landing.biolink.ontology",
+            "landing.disease_ontology.ontology",
+            "landing.gene_ontology.ontology",
+            "landing.human_phenotype_ontology.ontology",
+            "landing.mondo.ontology",
+            "landing.orphanet.ontology",
+            "landing.uber_anatomy_ontology.ontology",
+        ],
     )
