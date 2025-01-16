788fce4647313772cce819d79430eb515cf4025e
Lucas Vittor <lvvittor@gmail.com>
2025-01-16 13:18:40 -0800

    [RFC] feat: add gene_names pipeline for landing->bronze (#3)
    * feat(datasets): add gene_names landing zone
    
    * chore(ncbigene): wip gene2goreader pipeline
    
    * feat(pipeline, datasets): add gene2goreader dataset ad ncbigene landing->bronze step

--- src/optimuskg/pipelines/bronze/nodes/__init__.py
+++ src/optimuskg/pipelines/bronze/nodes/__init__.py
@@ -1,5 +1,14 @@
 from .bgee import bgee_node
 from .ctd import ctd_node
+from .gene_names import gene_names_node
+from .ncbigene import gene2go_node
 from .reactome import reactome_ncbi_node, reactome_pathways_node
 
-__all__ = ["bgee_node", "ctd_node", "reactome_ncbi_node", "reactome_pathways_node"]
+__all__ = [
+    "bgee_node",
+    "ctd_node",
+    "reactome_ncbi_node",
+    "reactome_pathways_node",
+    "gene_names_node",
+    "gene2go_node",
+]
