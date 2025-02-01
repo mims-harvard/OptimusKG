9ca36ebcea82e2164c2f60cab55def9c449813da
Lucas Vittor <lvvittor@gmail.com>
2025-02-01 00:18:50 -0300

    chore: update clean files script

--- scripts/clean_files.sh
+++ scripts/clean_files.sh
@@ -4,13 +4,15 @@ rm -rf `find . -name __pycache__`
 rm -f `find . -type f -name '*.py[co]'`
 rm -f `find . -type f -name '*~'`
 rm -f `find . -type f -name '.*~'`
+rm -f `find . -type f -name '*.log'`
+rm -rf `find . -type d -name '.ipynb_checkpoints'`
+rm -rf `find . -type d -name '*.egg-info'`
 rm -rf .cache
+rm -rf dist
 rm -rf .mypy_cache
 rm -rf .venv
 rm -rf .pytest_cache
 rm -rf .ruff_cache
-rm -rf .ipynb_checkpoints
 rm -rf htmlcov
-rm -rf *.egg-info
 rm -f .coverage
 rm -f .coverage.*
