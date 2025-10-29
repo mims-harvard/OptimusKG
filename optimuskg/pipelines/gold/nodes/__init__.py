3e27a8af894c3366a0d52ae955b78d7b3aa4444b
Lucas Vittor <lvvittor@gmail.com>
2025-10-28 20:52:23 -0400

    bump version

--- optimuskg/pipelines/gold/nodes/__init__.py
+++ optimuskg/pipelines/gold/nodes/__init__.py
@@ -1,6 +1,6 @@
 from .neo4j_export import neo4j_export_node
-from .pg_export import pg_export_node
 from .parquet_export import parquet_export_node
+from .pg_export import pg_export_node
 
 __all__ = [
     "neo4j_export_node",
