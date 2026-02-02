ba02995b1f1898e717d38af726f1399440dcaa46
Lucas Vittor <lvvittor@gmail.com>
2026-02-02 17:46:08 -0500

    chore: remove unused parameters

--- optimuskg/pipelines/bronze/nodes/bgee.py
+++ optimuskg/pipelines/bronze/nodes/bgee.py
@@ -8,8 +8,6 @@ logger = logging.getLogger(__name__)
 
 def run(
     homo_sapiens_expressions_advanced: pl.DataFrame,
-    expression_rank_threshold: int,
-    call_quality: str,
 ) -> pl.DataFrame:
     df = homo_sapiens_expressions_advanced.filter(
         pl.col("Anatomical entity ID").str.starts_with("UBERON")
@@ -28,13 +26,8 @@ def run(
     )
 
     df = df.filter(
-        # TODO: filtering for expression rank/call quality should have a
-        # parameter to enable/disable it (to have large/small versions of the final KG)
-        # (pl.col("call_quality") == call_quality)
-        # & (pl.col("expression_rank") < expression_rank_threshold)
-        # & (
         ~pl.col("anatomy_id").str.contains("∩")
-        # )  # NOTE: New versions of the dataset include measurements with an intersection of tissues. We want to remove this measurements.
+        # NOTE: New versions of the dataset include measurements with an intersection of tissues. We want to remove this measurements.
     )
 
     logger.debug(f"Wrote {len(df)} anatomy-gene pairs")
@@ -46,8 +39,6 @@ bgee_node = node(
     run,
     inputs={
         "homo_sapiens_expressions_advanced": "landing.bgee.homo_sapiens_expressions_advanced",
-        "expression_rank_threshold": "params:expression_rank_threshold",
-        "call_quality": "params:call_quality",
     },
     outputs="bgee.gene_expressions_in_anatomy",
     name="bgee",
