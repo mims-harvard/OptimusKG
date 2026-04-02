dd67b50ce05789aff96a36efa12bab806902956b
Iñaki Arango <arangoinaki@gmail.com>
2026-04-01 18:02:57 -0700

    fix build

---
+++ docs/src/lib/shared.ts
@@ -0,0 +1,10 @@
+export const appName = 'OptimusKG';
+export const docsRoute = '/docs';
+export const docsImageRoute = '/og/docs';
+export const docsContentRoute = '/llms.mdx/docs';
+
+export const gitConfig = {
+  user: 'mims-harvard',
+  repo: 'optimuskg',
+  branch: 'main',
+};
