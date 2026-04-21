2c08445b230d1b0c09ae59b7fdd8ea893859bc69
Iñaki Arango <arangoinaki@gmail.com>
2026-04-20 23:24:19 -0700

    prune website

--- docs/src/app/(home)/page.tsx
+++ docs/src/app/(home)/page.tsx
@@ -1,8 +1,6 @@
 import { FeaturesSection } from "./_components/features-section";
 import { Footer } from "./_components/footer";
-import { FrontierSection } from "./_components/frontier-section";
 import { HeroSection } from "./_components/hero-section";
-import { LogoGarden } from "./_components/logo-garden";
 import { Navbar } from "./_components/navbar";
 import { StatsScroller } from "./_components/stats-scroller";
 
@@ -13,9 +11,7 @@ export default function HomePage() {
       <main>
         <HeroSection />
         <StatsScroller />
-        {/* <LogoGarden /> */}
         <FeaturesSection />
-        {/* <FrontierSection /> */}
       </main>
       <Footer />
     </>
